123013ac33fb5d0b441e8118e834d81a196b76b9
Cho Cheung <105308452+Thingsthing@users.noreply.github.com>
2022-07-29 15:26:45 -0400

    x

---
+++ Palindrome Number/PalindromeNumber/src/Solution.java
@@ -0,0 +1,24 @@
+//Cho Cheung
+//07/28/200
+//LeetCode Project
+//Palindrome Number
+
+
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 121;
+        boolean isPalindrome;
+        int[] numsInput = new int[x];
+        System.out.println(numsInput);
+        System.out.println("is this palindrome ");
+    }
+
+        public static boolean IsPalindrome(int x) {
+            int[] numsInput = new int[x];
+            System.out.println(numsInput);
+            return IsPalindrome(x);
+        }
+
+
+}
