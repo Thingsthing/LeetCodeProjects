559c4172846f7af23724936d616bd9ccebc3f33c
Cho Cheung <105308452+Thingsthing@users.noreply.github.com>
2022-07-29 17:44:37 -0400

    done

--- Palindrome Number/PalindromeNumber/src/Solution.java
+++ Palindrome Number/PalindromeNumber/src/Solution.java
@@ -6,19 +6,25 @@
 
 
 public class Solution {
-    public static void main(String[] args) {
-        int x = 121;
-        boolean isPalindrome;
-        int[] numsInput = new int[x];
-        System.out.println(numsInput);
-        System.out.println("is this palindrome ");
-    }
 
-        public static boolean IsPalindrome(int x) {
-            int[] numsInput = new int[x];
-            System.out.println(numsInput);
-            return IsPalindrome(x);
+    public static boolean isPalindrome (int x) {
+        if (x == 0) {
+            return true;
         }
-
-
+        if (x < 0 || x % 10 == 0) {
+            return false;
+        }
+        int reversed_int = 0;
+        while(x > reversed_int) {
+            int pop = x % 10; //grab last digit
+            x /= 10; //chopping of last digit
+            reversed_int = (reversed_int * 10) + pop; //putting that digit into int
+        }
+        if(x == reversed_int || x == reversed_int / 10) { //check for odd digits
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
